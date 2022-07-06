af7a385f5bfb58ed441a3720573dd53c59af6519
Sergey <santexnik.86@mail.ru>
2022-07-06 18:44:50 +0300

    Задача 27

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿void Zadacha27()
+{ 
+    Console.WriteLine("Введите число ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($" сумма цифр в числе {number}  = {FindSumm(number)}");
+}
+
+int FindSumm(int number)
+{
+    
+    int sumnum= 0;
+    while(number >0)
+    {
+        sumnum =sumnum + number%10;
+        number=number/10;  
+    }    
+    return sumnum;
+}    
+Zadacha27();
